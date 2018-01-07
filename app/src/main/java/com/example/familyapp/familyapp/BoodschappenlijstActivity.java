b72392fbd8e13945f7642d4980b848d4c94f7f71
StijnMets <metsstijn@gmail.com>
2018-01-07 22:52:16 +0100

    boodschappenlijst met toolbar 2

--- app/src/main/java/com/example/familyapp/familyapp/BoodschappenlijstActivity.java
+++ app/src/main/java/com/example/familyapp/familyapp/BoodschappenlijstActivity.java
@@ -1,14 +1,19 @@
 package com.example.familyapp.familyapp;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,6 +31,7 @@ public class BoodschappenlijstActivity extends AppCompatActivity {
     private ListView listview;
     private EditText editText;
     private Button button;
+    private ImageView imageView;
 
     private ArrayList<String> arrayList = new ArrayList<>();
     private ArrayAdapter<String> adapter;
@@ -35,20 +41,48 @@ public class BoodschappenlijstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boodschappenlijst);
 
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.CustomToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Boodschappenlijst");
+        getSupportActionBar().setIcon(getDrawable(R.drawable.ic_addwhite));*/
+
+
+
         dref = FirebaseDatabase.getInstance().getReference("Boodschappenlijst");
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
 
-        editText = (EditText)findViewById(R.id.txt_add);
         listview = (ListView)findViewById(R.id.listview);
-        button = (Button) findViewById(R.id.btn_add);
+        imageView = (ImageView) findViewById(R.id.img_Add);
+
 
         listview.setAdapter(adapter);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dref.push().setValue(editText.getText().toString());
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(BoodschappenlijstActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_boodschappen, null);
+                final EditText mProduct = (EditText) mView.findViewById(R.id.etProduct);
+                Button mToevoegen = (Button) mView.findViewById(R.id.btn_toevoegen);
+
+                mToevoegen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!mProduct.getText().toString().isEmpty()){
+                            Toast.makeText(BoodschappenlijstActivity.this, "Product toegevoegd", Toast.LENGTH_SHORT).show();
+                            dref.push().setValue(mProduct.getText().toString());
+                        }
+                        else{
+                            Toast.makeText(BoodschappenlijstActivity.this, "Vul een product in", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                
             }
         });
 
