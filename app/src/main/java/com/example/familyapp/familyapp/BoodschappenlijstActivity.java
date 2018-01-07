7a1299047772e525408ff12fce6776014c6a5c54
StijnMets <metsstijn@gmail.com>
2018-01-07 16:14:00 +0100

    boodschappenlijst toevoegen

---
+++ app/src/main/java/com/example/familyapp/familyapp/BoodschappenlijstActivity.java
@@ -0,0 +1,13 @@
+package com.example.familyapp.familyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoodschappenlijstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boodschappenlijst);
+    }
+}
